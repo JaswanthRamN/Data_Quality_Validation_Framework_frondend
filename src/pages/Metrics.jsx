1f8062dbd0991736121e4e64047b8bc09f2e1be2
JaswanthRamN <jaswanthram18@gmail.com>
2026-05-18 14:41:40 +0530

    Add Metrics.jsx page component for metrics display

---
+++ src/pages/Metrics.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Metrics() {
+  return (
+    <div className="metrics-container">
+      <h1>Metrics</h1>
+      <p>Metrics page content goes here</p>
+    </div>
+  );
+}
